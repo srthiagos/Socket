44f3fb42cb01135a34f5d6939f46bef7a40962f6
srthiagos <messiasthi@gmail.com>
2020-11-30 13:12:27 -0300

    Add some comments

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const socket = require('socket.io');
 const path = require('path');
 const fs = require('fs');
 
+// Serve the index.html file in root path
 const root = (req, res) => {
   res.writeHead(200);
   const index = fs.createReadStream(path.join(__dirname, 'index.html'));
   index.pipe(res);
 };
-
+// Create server
 const server = require('http').createServer(root);
+// Start server and set to listen port 8080
 server.listen(8080, () => {
   console.log('Start server in http://localhost:8080/');
 });
